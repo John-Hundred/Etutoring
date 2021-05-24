9f510a96bd97ad1bde7ab8f5e4ccd08b6d0217a5
luca <luca.marignati@edu.unito.it>
2021-05-24 16:42:05 +0200

    fix login php api

--- lib/utils/user_secure_storage.dart
+++ lib/utils/user_secure_storage.dart
@@ -4,8 +4,6 @@ class UserSecureStorage {
   static final _storage = FlutterSecureStorage();
 
   static const _keyEmail = 'email';
-  /*static const _keyPets = 'pets';
-  static const _keyBirthday = 'birthday';*/
   static const _keyPassword = 'password';
 
   static Future setEmail(String email) async =>
@@ -18,28 +16,4 @@ class UserSecureStorage {
 
   static Future<String> getPassword() async =>
       await _storage.read(key: _keyPassword);
-
-  /*static Future setPets(List<String> pets) async {
-    final value = json.encode(pets);
-
-    await _storage.write(key: _keyPets, value: value);
-  }
-
-  static Future<List<String>> getPets() async {
-    final value = await _storage.read(key: _keyPets);
-
-    return value == null ? null : List<String>.from(json.decode(value));
-  }
-
-  static Future setBirthday(DateTime dateOfBirth) async {
-    final birthday = dateOfBirth.toIso8601String();
-
-    await _storage.write(key: _keyBirthday, value: birthday);
-  }
-
-  static Future<DateTime> getBirthday() async {
-    final birthday = await _storage.read(key: _keyBirthday);
-
-    return birthday == null ? null : DateTime.tryParse(birthday);
-  }*/
 }
