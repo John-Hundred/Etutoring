0efadb318816d90e43cb8881d6cff451f407c733
luca <luca.marignati@edu.unito.it>
2021-05-24 15:42:34 +0200

    fix login php api

--- lib/screens/user_page.dart
+++ lib/screens/user_page.dart
@@ -3,6 +3,10 @@ import 'package:argon_flutter/widgets/title_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:argon_flutter/utils/user_secure_storage.dart';
 
+import 'package:argon_flutter/screens/profileUser.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class UserPage extends StatefulWidget {
   @override
   _UserPageState createState() => _UserPageState();
@@ -32,6 +36,69 @@ class _UserPageState extends State<UserPage> {
     setState(() {
       _obscureText = !_obscureText;
     });
+  }
+
+  Future userLogin() async {
+    // Showing CircularProgressIndicator.
+    setState(() {
+      visible = true;
+    });
+
+    // Getting value from Controller
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    // SERVER LOGIN API URL
+    var url = 'e-tutoring-web/ws/ws/user_login.php';
+
+    // Store all data with Param Name.
+    var data = {'email': email, 'password': password};
+
+    // Starting Web API Call.
+    var response =
+        await http.post(Uri.http("192.168.56.1", url), body: json.encode(data));
+
+    // Getting Server response into variable.
+    var message = jsonDecode(response.body);
+
+    // If the Response Message is Matched.
+    if (message == 'Login Matched') {
+      // Hiding the CircularProgressIndicator.
+      setState(() {
+        visible = false;
+      });
+
+      // Navigate to Profile Screen & Sending Email to Next Screen.
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  ProfileScreen(email: emailController.text)));
+    } else {
+      // If Email or Password did not Matched.
+      // Hiding the CircularProgressIndicator.
+      setState(() {
+        visible = false;
+      });
+
+      // Showing Alert Dialog with Response JSON Message.
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: new Text(message),
+            actions: <Widget>[
+              TextButton(
+                child: new Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   Future init() async {
@@ -76,7 +143,7 @@ class _UserPageState extends State<UserPage> {
             const SizedBox(height: 32),*/
             buildButton(),
             Visibility(
-              visible: false,
+              visible: visible,
               child: Center(
                   child: Container(
                       margin: EdgeInsets.only(bottom: 30),
@@ -163,8 +230,9 @@ class _UserPageState extends State<UserPage> {
           // you'd often call a server or save the information in a database.
           await UserSecureStorage.setUsername(emailController.text);
           await UserSecureStorage.setPassword(passwordController.text);
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text('Login and Save Data')));
+          userLogin();
+          /*ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('Login and Save Data')));*/
         }
 
         /*await UserSecureStorage.setPets(pets);
