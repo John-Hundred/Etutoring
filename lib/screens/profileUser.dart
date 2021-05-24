84aaa704efac55d85f8c79280f17f145ea6165d7
luca <luca.marignati@edu.unito.it>
2021-05-24 17:00:17 +0200

    fix login php api

--- lib/screens/profileUser.dart
+++ lib/screens/profileUser.dart
@@ -48,6 +48,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
     this.setState(() {
       init();
     });
+
+    final Future<String> _calculation = Future<String>.delayed(
+      const Duration(seconds: 2),
+      () => 'Data Loaded',
+    );
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         routes: <String, WidgetBuilder>{
@@ -68,7 +74,54 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
             drawer: ArgonDrawer(currentPage: "Profilo"),
             body: Center(
-                child: Column(
+              child: FutureBuilder<String>(
+                future: _calculation,
+                builder:
+                    (BuildContext context, AsyncSnapshot<String> snapshot) {
+                  List<Widget> children;
+                  if (snapshot.hasData) {
+                    children = <Widget>[
+                      const Icon(
+                        Icons.check_circle_outline,
+                        color: Colors.green,
+                        size: 60,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16),
+                        child: Text('Result: ${snapshot.data}'),
+                      )
+                    ];
+                  } else if (snapshot.hasError) {
+                    children = <Widget>[
+                      const Icon(
+                        Icons.error_outline,
+                        color: Colors.red,
+                        size: 60,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16),
+                        child: Text('Error: ${snapshot.error}'),
+                      )
+                    ];
+                  } else {
+                    children = const <Widget>[
+                      SizedBox(
+                        child: CircularProgressIndicator(),
+                        width: 60,
+                        height: 60,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 16),
+                        child: Text('Awaiting result...'),
+                      )
+                    ];
+                  }
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: children,
+                      /*Column(
               children: <Widget>[
                 Container(
                     width: 280,
@@ -83,6 +136,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   child: Text('Click Here To Logout'),
                 ),
               ],
-            ))));
+            )*/
+                    ),
+                  );
+                },
+              ),
+            )));
   }
 }
