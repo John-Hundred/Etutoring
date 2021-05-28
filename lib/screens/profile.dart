039491b88d558c9916404b7e8afaa017934a14d3
luca <luca.marignati@edu.unito.it>
2021-05-28 09:58:28 +0200

    fix login

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -424,6 +424,62 @@ class _ProfileState extends State<Profile> {
                                                   indent: 32.0,
                                                   endIndent: 32.0,
                                                 ),
+                                                DataTable(
+                                                  columns: const <DataColumn>[
+                                                    DataColumn(
+                                                      label: Text(
+                                                        'Name',
+                                                        style: TextStyle(
+                                                            fontStyle: FontStyle
+                                                                .italic),
+                                                      ),
+                                                    ),
+                                                    DataColumn(
+                                                      label: Text(
+                                                        'Age',
+                                                        style: TextStyle(
+                                                            fontStyle: FontStyle
+                                                                .italic),
+                                                      ),
+                                                    ),
+                                                    DataColumn(
+                                                      label: Text(
+                                                        'Role',
+                                                        style: TextStyle(
+                                                            fontStyle: FontStyle
+                                                                .italic),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                  rows: const <DataRow>[
+                                                    DataRow(
+                                                      cells: <DataCell>[
+                                                        DataCell(Text('Sarah')),
+                                                        DataCell(Text('19')),
+                                                        DataCell(
+                                                            Text('Student')),
+                                                      ],
+                                                    ),
+                                                    DataRow(
+                                                      cells: <DataCell>[
+                                                        DataCell(
+                                                            Text('Janine')),
+                                                        DataCell(Text('43')),
+                                                        DataCell(
+                                                            Text('Professor')),
+                                                      ],
+                                                    ),
+                                                    DataRow(
+                                                      cells: <DataCell>[
+                                                        DataCell(
+                                                            Text('William')),
+                                                        DataCell(Text('27')),
+                                                        DataCell(Text(
+                                                            'Associate Professor')),
+                                                      ],
+                                                    ),
+                                                  ],
+                                                ),
                                                 Padding(
                                                   padding:
                                                       const EdgeInsets.only(
