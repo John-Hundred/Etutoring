c423268003e24563a873f2ba5faba370c51ac1e0
luca <luca.marignati@edu.unito.it>
2021-06-04 16:04:01 +0200

    fix my tutoring course

--- lib/screens/courseDetail.dart
+++ lib/screens/courseDetail.dart
@@ -22,12 +22,54 @@ class CourseDetailState extends State<CourseDetail> {
     // print(this.courseData);
   }
 
+  /*Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Are you sure?'),
+            content: new Text('Do you want to exit an App'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: new Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }*/
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: AppBar(
             title: Text(courseData.course_name.toString().capitalize()),
             backgroundColor: Color.fromRGBO(213, 21, 36, 1)),
+        // drawer: ArgonDrawer("courseDetail"),
+        body: Container());
+    /*return new WillPopScope(
+      onWillPop: _onWillPop,
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Home Page"),
+        ),
+        body: new Center(
+          child: new Text("Home Page"),
+        ),
+      ),
+    );*/
+  }
+
+  /*Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text(courseData.course_name.toString().capitalize()),
+            backgroundColor: Color.fromRGBO(213, 21, 36, 1)),
         drawer: ArgonDrawer("courseDetail"),
         body: Container());
-  }
+  }*/
 }
