f44ce83e04497cdccfc78d3d6025987d168584c8
luca <luca.marignati@edu.unito.it>
2021-06-07 14:33:09 +0200

    fix profile and settings

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -21,27 +21,43 @@ class _SettingsState extends State<Settings> {
           backgroundColor: Color.fromRGBO(213, 21, 36, 1)),
       // Nav Bar (title: 'Profilo', bgColor: Color.fromRGBO(213, 21, 36, 1)),
       drawer: ArgonDrawer("settings"),
-      body: SizedBox(
-        // width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.only(left: 34.0, right: 34.0, top: 8),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size.fromHeight(52),
-              primary: ArgonColors.redUnito,
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25),
-              ),
-            ),
-            onPressed: () {},
-            child: Padding(
-                padding: EdgeInsets.only(
-                    left: 16.0, right: 16.0, top: 12, bottom: 12),
-                child: Text("Delete User", style: TextStyle(fontSize: 16.0))),
-          ),
-        ),
-      ),
+      body: Stack(children: <Widget>[
+        SafeArea(
+            child: ListView(children: [
+          Padding(
+              padding:
+                  const EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 34.0, right: 34.0, top: 8),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size.fromHeight(52),
+                            primary: ArgonColors.redUnito,
+                            elevation: 5,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Padding(
+                              padding: EdgeInsets.only(
+                                  left: 16.0, right: 16.0, top: 12, bottom: 12),
+                              child: Text("Delete User",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16.0))),
+                        ),
+                      ),
+                    ),
+                  ]))
+        ]))
+      ]),
     );
   }
 }
