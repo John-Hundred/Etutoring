643cf674e73af58b1be1d0544d3dde71b4923852
luca <luca.marignati@edu.unito.it>
2021-06-07 14:23:06 +0200

    fix profile and settings

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,3 +1,4 @@
+import 'package:argon_flutter/constants/Theme.dart';
 import 'package:argon_flutter/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
@@ -15,11 +16,32 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text('Settings'),
-            backgroundColor: Color.fromRGBO(213, 21, 36, 1)),
-        // Nav Bar (title: 'Profilo', bgColor: Color.fromRGBO(213, 21, 36, 1)),
-        drawer: ArgonDrawer("settings"),
-        body: Column());
+      appBar: AppBar(
+          title: const Text('Settings'),
+          backgroundColor: Color.fromRGBO(213, 21, 36, 1)),
+      // Nav Bar (title: 'Profilo', bgColor: Color.fromRGBO(213, 21, 36, 1)),
+      drawer: ArgonDrawer("settings"),
+      body: SizedBox(
+        // width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 34.0, right: 34.0, top: 8),
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size.fromHeight(52),
+              primary: ArgonColors.redUnito,
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+            ),
+            onPressed: () {},
+            child: Padding(
+                padding: EdgeInsets.only(
+                    left: 16.0, right: 16.0, top: 12, bottom: 12),
+                child: Text("Delete User", style: TextStyle(fontSize: 16.0))),
+          ),
+        ),
+      ),
+    );
   }
 }
