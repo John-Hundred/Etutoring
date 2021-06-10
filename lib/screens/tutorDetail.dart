9bd6bd5a969fbab5058ee0859a5c564ffdc5d9d1
luca <luca.marignati@edu.unito.it>
2021-06-10 15:03:18 +0200

    fix tutor page

--- lib/screens/tutorDetail.dart
+++ lib/screens/tutorDetail.dart
@@ -1,4 +1,3 @@
-import 'package:e_tutoring/constants/Theme.dart';
 import 'package:e_tutoring/widgets/language_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
