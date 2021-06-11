55406785e75e2104a0dbab201a2e6d315aa96f5b
luca <luca.marignati@edu.unito.it>
2021-06-11 15:02:23 +0200

    feat chat wip

--- lib/screens/chatDetail.dart
+++ lib/screens/chatDetail.dart
@@ -15,12 +15,73 @@ class _ChatDetailState extends State<ChatDetail> {
         resizeToAvoidBottomInset: false,
         backgroundColor: Color.fromRGBO(205, 205, 205, 1),
         appBar: AppBar(
+          // title: Text(AppLocalizations.of(context).chat),
+          backgroundColor: Color.fromRGBO(213, 21, 36, 1),
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: EdgeInsets.only(right: 16),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"),
+                    maxRadius: 20,
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Luca Marignati",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Text(
+                          "Online",
+                          style: TextStyle(fontSize: 13, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        /* appBar: AppBar(
           title: Text(AppLocalizations.of(context).chat),
           backgroundColor: Color.fromRGBO(213, 21, 36, 1),
           actions: <Widget>[
             // LanguagePickerWidget(),
           ],
-        ),
+        ),*/
         drawer: ArgonDrawer("chat-detail"),
         body: Container());
   }
