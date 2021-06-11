6bddf3183629148cef6e08117a7ea0d2e91357b4
luca <luca.marignati@edu.unito.it>
2021-06-11 15:57:55 +0200

    fix reviews

---
+++ lib/model/reviewModel.dart
@@ -0,0 +1,30 @@
+class ReviewModel {
+  // ignore: non_constant_identifier_names
+  String review_id;
+  // ignore: non_constant_identifier_names
+  String user_tutor_id;
+  // ignore: non_constant_identifier_names
+  String user_id;
+  // ignore: non_constant_identifier_names
+  String review_star;
+  // ignore: non_constant_identifier_names
+  String review_comment;
+
+  ReviewModel(this.review_id, this.user_tutor_id, this.user_id,
+      this.review_star, this.review_comment);
+
+  ReviewModel.fromJson(dynamic json) {
+    review_id = json['review_id'] ?? '-';
+    user_tutor_id = json['user_tutor_id'] ?? '-';
+    user_id = json['user_id'] ?? '-';
+    review_star = json['review_star'] ?? '-';
+    review_comment = json['review_comment'] ?? '-';
+  }
+
+  toString() {
+    return "review_id = " +
+        this.review_id +
+        "\nreview_comment = " +
+        this.review_comment;
+  }
+}
