afd45e281236bcdddadd36038be2df7c4a0b8294
luca <luca.marignati@edu.unito.it>
2021-06-15 18:28:48 +0200

    fix profile edit

--- lib/screens/profile-edit.dart
+++ lib/screens/profile-edit.dart
@@ -28,7 +28,134 @@ class _ProfileStateEdit extends State<ProfileEdit> {
                   BoxDecoration(color: Color.fromRGBO(205, 205, 205, 1))),
           SafeArea(
               child: Scaffold(
-                  body: ListView(padding: EdgeInsets.all(8), children: [])))
+                  body: ListView(padding: EdgeInsets.all(8), children: [
+            Container(
+                width: 320,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Surname Here...',
+                    prefixIcon: Icon(Icons.person),
+                    hintStyle: TextStyle(color: Colors.black),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                  ),
+                )),
+            Container(
+                width: 320,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Name Here...',
+                    prefixIcon: Icon(Icons.person),
+                    hintStyle: TextStyle(color: Colors.black),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                  ),
+                )),
+            Container(
+                width: 320,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Nationality Here...',
+                    prefixIcon: Icon(Icons.flag),
+                    hintStyle: TextStyle(color: Colors.black),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                  ),
+                )),
+            Container(
+                width: 320,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Birth Place Here...',
+                    prefixIcon: Icon(Icons.place),
+                    hintStyle: TextStyle(color: Colors.black),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                  ),
+                )),
+            Container(
+                width: 320,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Residence Here...',
+                    prefixIcon: Icon(Icons.place),
+                    hintStyle: TextStyle(color: Colors.black),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                  ),
+                )),
+            Container(
+                width: 320,
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  autocorrect: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Your Email Here...',
+                    prefixIcon: Icon(Icons.email),
+                    hintStyle: TextStyle(color: Colors.black),
+                    filled: true,
+                    fillColor: Colors.white70,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                    ),
+                  ),
+                ))
+          ])))
         ]));
   }
 }
