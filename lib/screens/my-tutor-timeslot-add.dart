d219ab1c525496b81a6ac4c8d96d711dba11486f
luca <luca.marignati@edu.unito.it>
2021-06-19 21:41:34 +0200

    fix time slot add

--- lib/screens/my-tutor-timeslot-add.dart
+++ lib/screens/my-tutor-timeslot-add.dart
@@ -1,6 +1,7 @@
 import 'package:e_tutoring/constants/Theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:intl/intl.dart';
 
 class MyTutorTimeslotAdd extends StatefulWidget {
   @override
@@ -71,7 +72,7 @@ class MyTutorTimeslotAddState extends State<MyTutorTimeslotAdd> {
     final DateTime pickedDate = await showDatePicker(
         context: context,
         initialDate: currentDate,
-        firstDate: DateTime(2015),
+        firstDate: DateTime(2020),
         lastDate: DateTime(2050),
         builder: (BuildContext context, Widget child) {
           return Theme(
@@ -92,6 +93,12 @@ class MyTutorTimeslotAddState extends State<MyTutorTimeslotAdd> {
       });
   }
 
+  String formatDate(date) {
+    final DateFormat formatter = DateFormat('dd-MM-yyyy');
+    String formatted = formatter.format(date);
+    return formatted;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -110,7 +117,7 @@ class MyTutorTimeslotAddState extends State<MyTutorTimeslotAdd> {
                 style: TextStyle(color: ArgonColors.redUnito)),
             style: ElevatedButton.styleFrom(primary: Colors.white70),
           ),
-          Text(currentDate.toString()),
+          Text(formatDate(currentDate).toString()),
           ElevatedButton(
               onPressed: () => _selectTimeFrom(context),
               child: Text('Select from hour',
@@ -118,7 +125,7 @@ class MyTutorTimeslotAddState extends State<MyTutorTimeslotAdd> {
               style: ElevatedButton.styleFrom(primary: Colors.white70)),
           SizedBox(height: 8),
           Text(
-            'Selected time: ${_timeFrom.format(context)}',
+            '${_timeFrom.format(context)}',
           ),
           ElevatedButton(
               onPressed: () => _selectTimeTo(context),
@@ -127,7 +134,7 @@ class MyTutorTimeslotAddState extends State<MyTutorTimeslotAdd> {
               style: ElevatedButton.styleFrom(primary: Colors.white70)),
           SizedBox(height: 8),
           Text(
-            'Selected time: ${_timeTo.format(context)}',
+            '${_timeTo.format(context)}',
           ),
         ],
       ),
